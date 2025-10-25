7c37b8361260130463f540a1c3ccd5f9d18348b5
Aayisha Sidhik <aayishasidhik321@gmail.com>
2025-10-25 20:28:00 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar.jsx';
-import Home from './components/Home.jsx';
-import EmployeeForm from './components/EmployeeForm.jsx';
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import EmployeeForm from "./components/EmployeeForm";
 
-export default function App() {
+function App() {
+  const [employees, setEmployees] = useState([]);
+
+  const addEmployee = (employee) => {
+    const newEmployee = { id: employees.length + 1, ...employee };
+    setEmployees([...employees, newEmployee]);
+  };
+
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/add-employee' element={<EmployeeForm />} />
+        <Route path="/" element={<Home employees={employees} />} />
+        <Route path="/employee-form" element={<EmployeeForm addEmployee={addEmployee} />} />
       </Routes>
     </Router>
   );
 }
+
+export default App;
